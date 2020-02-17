734801e363e1f6002340bbb00f7c1684a3273252
Sara Echeverria <saraecheverria@Saras-MacBook-Pro.local>
2020-02-17 10:21:04 -0500

    committ

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,3 +1,28 @@
 export function Sum (a, b) {
-  return a + b
+  if (typeof a == 'number' && typeof b == 'number')
+  return (a+b); 
+  else 
+  return undefined; 
+}
+
+export function AddList(list){ 
+  var result = 0; 
+  for (var mem of list){ 
+
+  }
+  for (var i = 0; i < list.length; i++){ 
+    if (list[i] == undefined) 
+    result = result + list[i]; 
+
+  }
+  return 0; 
+}
+
+let op1 = "Ten";  
+let op2 = 2; 
+let result = Sum(op1,op2); 
+if (result==op1+op2) { 
+    console.log("It WORKED!!!")
+} else { 
+  console.log("Expected " + op1+op2 + ", but got " + result); 
 }
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,27 +1,40 @@
-import { Sum } from './sum.js'
+import { Sum } from './MyMath.js'
+describe('Testing Sum',() => {
+it("Adds a string and a number",()=>{
+   let list = [1,2,3,4]; 
+   let result = AddList(list); 
+   expect(result).toBe(10);
+});
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(Sum(1, 2)).toBe(3)
+  expect(Sum(1, 2)).toBe(3) })
+test('Border cases', ()=>{
+  expect(Sum(-10, 5)).toBe(-5)
+  //expect(Sum(.1, .2)).toBe(.3)
+  expect(Sum(100000, 100000)).toBe(200000)
 })
-
+});
 // ICE 
 
 // Examples for Null
 test('null', () => {
-  const n = null
-  expect(n).toBeNull()
-  expect(n).toBeDefined()
-  expect(n).not.toBeUndefined()
-  expect(n).not.toBeTruthy()
-  expect(n).toBeFalsy()
+  const n = null
+  expect(n).toBeNull()
+  expect(n).toBeDefined()
+  expect(n).not.toBeUndefined()
+  expect(n).not.toBeTruthy()
+  expect(n).toBeFalsy()
 })
 
 // Examples for zero
 test('zero', () => {
-  const z = 0
-  expect(z).not.toBeNull()
-  expect(z).toBeDefined()
-  expect(z).not.toBeUndefined()
-  expect(z).not.toBeTruthy()
-  expect(z).toBeFalsy()
+  const z = 0
+  expect(z).not.toBeNull()
+  expect(z).toBeDefined()
+  expect(z).not.toBeUndefined()
+  expect(z).not.toBeTruthy()
+  expect(z).toBeFalsy()
 })
+
+
+
