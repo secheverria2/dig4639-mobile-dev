aa73298058357404e378ae436346953cb9f5f933
Sara Echeverria <saraecheverria@Saras-MacBook-Pro.local>
2020-03-01 16:12:55 -0500

    commit

--- labs/3/src/components/Card/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "./index.css";
-
-class Card extends React.Component {
-    render() {
-        return (<p className="card">This is a card</p>);
-      }
-    }
-export default Card;
---
+++ labs/4/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CardList from './components/CardList/index.js'; 
+
+class App extends
+React.Component { 
+  render() { 
+    return ( 
+      <CardList/> 
+    )
+  }
+}
+
+export default App; 
