38148e3103f742d9a4aea5fc59030c92c0722de0
Sara Echeverria <saraecheverria@Saras-MacBook-Pro.local>
2020-03-28 16:17:15 -0400

    lab 5

--- labs/5/src/App.js
+++ labs/5/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CardList from './components/CardList/index.js'
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {content:"", list: []}
+  }
+  
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <CardList 
+          data={this.state.list}
+          >
+          </CardList>
+    )
+  }
 }
+
 
 export default App;
